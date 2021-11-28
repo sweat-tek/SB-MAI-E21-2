518635b036c5cf68a352dd21ee996753727fb026
Andreas Edal Pedersen <anepe19@student.sdu.dk>
2021-11-28 16:05:59 +0100

    Acceptance test for copy action

---
+++ JHotDraw/src/test/java/org/jhotdraw/copy/CopyBehaviorTest.java
@@ -0,0 +1,16 @@
+package org.jhotdraw.copy;
+
+import com.tngtech.jgiven.junit.ScenarioTest;
+import org.junit.Test;
+
+public class CopyBehaviorTest extends ScenarioTest<GivenSelectedFigure, WhenCopying, ThenFigureInClipboard> {
+
+    @Test
+    public void selectingSimpleFiguresAndCopyingThemResultsInJustThoseFiguresInTheClipboard() {
+        given().aSelectedFigure();
+
+        when().iCopy();
+
+        then().theSelectedFigureShouldBeInTheClipboard();
+    }
+}
